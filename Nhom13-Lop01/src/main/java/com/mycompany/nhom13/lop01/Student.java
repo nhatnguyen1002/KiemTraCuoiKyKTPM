0b4057da587138ee86604f0027f189513a7b9ed0
nhatnguyen1002 <nhatnguyen10022005@gmail.com>
2025-11-04 08:45:09 +0700

    Add files via upload

---
+++ Nhom13-Lop01/src/main/java/com/mycompany/nhom13/lop01/Student.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.nhom13.lop01;
+
+/**
+ *
+ * @author Admin
+ */
+public class Student {
+    private String id;
+    private String name;
+    
+    
+    //constructors
+    
+    //getters and setters
+}
---
+++ Nhom13-Lop01/src/test/java/com/mycompany/nhom13/lop01/StudentTest.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.nhom13.lop01;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Admin
+ */
+public class StudentTest {
+    
+    public StudentTest() {
+    }
+
+    @org.junit.jupiter.api.BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    public void tearDown() throws Exception {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @Test
+    public void testTC01(){
+        //......
+        assert
+    }
+    @org.junit.jupiter.api.Test
+    public void testSomeMethod() {
+        fail("The test case is a prototype.");
+    }
+    
+}
